601bafa6307d718d3d293abfcb80b2182c5429fe
Michael Anokwulu <112480000+hercjay@users.noreply.github.com>
2023-09-05 18:58:53 +0100

    data

---
+++ epiz_33153384_troway.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: sql210.infinityfree.com
+-- Generation Time: Sep 05, 2023 at 01:51 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.2.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `epiz_33153384_troway`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+-- Error reading structure for table epiz_33153384_troway.posts: #1142 - SHOW command denied to user 'epiz_33207701'@'192.168.0.6' for table 'posts'
+-- Error reading data for table epiz_33153384_troway.posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `epiz_33153384_troway`.`posts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_categories_map`
+--
+-- Error reading structure for table epiz_33153384_troway.post_categories_map: #1142 - SHOW command denied to user 'epiz_33207701'@'192.168.0.6' for table 'post_categories_map'
+-- Error reading data for table epiz_33153384_troway.post_categories_map: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `epiz_33153384_troway`.`post_categories_map`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_subcategories_map`
+--
+-- Error reading structure for table epiz_33153384_troway.post_subcategories_map: #1142 - SHOW command denied to user 'epiz_33207701'@'192.168.0.6' for table 'post_subcategories_map'
+-- Error reading data for table epiz_33153384_troway.post_subcategories_map: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `epiz_33153384_troway`.`post_subcategories_map`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table epiz_33153384_troway.users: #1142 - SHOW command denied to user 'epiz_33207701'@'192.168.0.6' for table 'users'
+-- Error reading data for table epiz_33153384_troway.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `epiz_33153384_troway`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
